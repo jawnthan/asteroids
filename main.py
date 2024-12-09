6c60c02fd206eb8d431f43351519898d858fa1b3
jawnthan <johnwwwhittaker@gmail.com>
2024-12-09 17:39:49 -0500

    added the ability to rotate

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
-    pygame.init()
     
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        dt = (clock.tick(60)) / 1000
+        player.update(dt)
+
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
